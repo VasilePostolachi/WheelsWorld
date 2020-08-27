4cc2db757bf63c440a9cdb30f400a323a9912e24
Vasile Postolachi <vasile_postolachi_1999@mail.ru>
2020-08-27 20:42:02 +0300

    FirstVersionOfKitINtegrationsInMVC

--- WheelsWorld/App_Start/BundleConfig.cs
+++ WheelsWorld/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace WheelsWorld
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/main").Include(
+                        "~/Scripts/main.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -24,6 +27,7 @@ namespace WheelsWorld
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/main.css",
                       "~/Content/site.css"));
         }
     }
